730e9853533735e951c2d293c7b74de0cdb9f123
Patrick Ambrose <patrickambrosericky@gmail.com>
2024-01-08 15:37:35 +0530

    Layout modifications

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,10 +7,10 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      "LinkedIn": "https://www.linkedin.com/in/PatrickAmbrosso",
-      "GitHub": "https://github.com/PatrickAmbrosso",
+      LinkedIn: "https://www.linkedin.com/in/PatrickAmbrosso",
+      GitHub: "https://github.com/PatrickAmbrosso",
       "Twitter (X)": "https://x.com/PatrickAmbrosso",
-      "Instagram": "https://www.instagram.com/PatrickAmbrosso"
+      Instagram: "https://www.instagram.com/PatrickAmbrosso",
     },
   }),
 }
@@ -28,48 +28,76 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.MobileOnly(Component.Darkmode()),
-    Component.DesktopOnly(Component.Explorer({
-      title: "Navigation",
-      folderDefaultState: "collapsed",
-      folderClickBehavior: "collapse",
-      useSavedState: false,
-      filterFn: (node) => {
-        // set containing names of everything you want to include
-        const include = new Set(["Musings","Reflections","Expeditions","Transcendence","Showcase"])
-        return include.has(node.name) // && !!node.file
-      },
-      sortFn: (a, b) => {
-        const nameOrderMap: Record<string, number> = {
-          "Musings": 100,
-          "Reflections": 200,
-          "Expeditions": 300,
-          "Transcendence": 400,
-          "Showcase": 500,
-        }
-     
-        let orderA = 0
-        let orderB = 0
-     
-        if (a.file && a.file.slug) {
-          orderA = nameOrderMap[a.file.slug] || 0
-        } else if (a.name) {
-          orderA = nameOrderMap[a.name] || 0
-        }
-     
-        if (b.file && b.file.slug) {
-          orderB = nameOrderMap[b.file.slug] || 0
-        } else if (b.name) {
-          orderB = nameOrderMap[b.name] || 0
-        }
-     
-        return orderA - orderB
-      },
-      order: ['filter','map','sort']
-    })),
-    Component.DesktopOnly(Component.RecentNotes({
-      title: "Recent Writings",
-      limit: 5,
-    }))
+    Component.DesktopOnly(
+      Component.Explorer({
+        title: "Navigation",
+        folderDefaultState: "collapsed",
+        folderClickBehavior: "collapse",
+        useSavedState: false,
+        filterFn: (node) => {
+          // set containing names of everything you want to include
+          const include = new Set([
+            "Musings",
+            "Reflections",
+            "Expeditions",
+            "Transcendence",
+            "Showcase",
+          ])
+          return include.has(node.name) // && !!node.file
+        },
+        mapFn: (node) => {
+          // don't change the name of the root node
+          if (node.depth > 0) {
+            // define a dictionary for mapping node names to icons
+            const iconMapping: {[key: string]: string} = {
+              Musings: "✨",
+              Reflections: "📖",
+              Expeditions: "🌏",
+              Transcendence: "🙏",
+              Showcase: "🌏",
+            }
+
+            // check if the node's name is in the dictionary
+            if (node.file && node.name in iconMapping) {
+              node.displayName = iconMapping[node.name] + " " + node.displayName
+            }
+          }
+        },
+        sortFn: (a, b) => {
+          const nameOrderMap: Record<string, number> = {
+            Musings: 100,
+            Reflections: 200,
+            Expeditions: 300,
+            Transcendence: 400,
+            Showcase: 500,
+          }
+
+          let orderA = 0
+          let orderB = 0
+
+          if (a.file && a.file.slug) {
+            orderA = nameOrderMap[a.file.slug] || 0
+          } else if (a.name) {
+            orderA = nameOrderMap[a.name] || 0
+          }
+
+          if (b.file && b.file.slug) {
+            orderB = nameOrderMap[b.file.slug] || 0
+          } else if (b.name) {
+            orderB = nameOrderMap[b.name] || 0
+          }
+
+          return orderA - orderB
+        },
+        order: ["filter", "map", "sort"],
+      }),
+    ),
+    Component.DesktopOnly(
+      Component.RecentNotes({
+        title: "Recent Writings",
+        limit: 5,
+      }),
+    ),
   ],
   right: [
     Component.DesktopOnly(Component.Darkmode()),
